e0a6f9ca8232fa063e8de193ebe4dcc6931f97fc
Bernadette <120725476+BernadetteWerneke@users.noreply.github.com>
2023-05-08 10:42:51 +0200

    Initial Commit

---
+++ Reiseziel/ReisezielApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReisezielApp.swift
+//  Reiseziel
+//
+//  Created by Bernadette Werneke on 08.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ReisezielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
